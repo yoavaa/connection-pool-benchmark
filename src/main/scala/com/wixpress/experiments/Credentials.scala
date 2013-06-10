6b2326648fc0ae537db92e3d4f98c7590fb40e8d
Yoav Abrahami <yoav@wix.com>
2013-06-10 20:00:02 +0300

    cleanup

--- src/main/scala/com/wixpress/experiments/Credentials.scala
+++ src/main/scala/com/wixpress/experiments/Credentials.scala
@@ -6,8 +6,8 @@ package com.wixpress.experiments
  * @since 5/6/13
  */
 object Credentials {
-  val jdbcUrl = "jdbc:mysql://localhost:3306/test"
-  val user = "root"
-  val pass = "picobo"
+  val jdbcUrl = "jdbc:mysql://localhost:3306/db"
+  val user = "user"
+  val pass = "pass"
   val driver = "com.mysql.jdbc.Driver"
 }
